39a2f99e1418cc65b7d2a7cb8909258a4f4b050c
mpfmorawski <mpfmorawski@gmail.com>
2021-06-15 13:10:29 +0200

    Updated plotting lines

--- lane_detector/plotLines.m
+++ lane_detector/plotLines.m
@@ -11,11 +11,7 @@ function [] = plotLines(lines)
     max_len = 0;
     for k = 1:length(lines)
        xy = [lines(k).point1; lines(k).point2];
-       plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
-
-       % Plot beginnings and ends of lines
-       plot(xy(1,1),xy(1,2),'x','LineWidth',2,'Color','yellow');
-       plot(xy(2,1),xy(2,2),'x','LineWidth',2,'Color','red');
+       plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','yellow');
 
        % Determine the endpoints of the longest line segment
        len = norm(lines(k).point1 - lines(k).point2);
